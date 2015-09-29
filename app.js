22af8961397a5078c835635840c13ae530ecfc6c
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-29 14:58:54 -0600

    added images for books with them

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
 require('dotenv').load()
 
 var app = express();
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/books', function(req, res) {
     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
       .end(function (response) {
         var books = response.body.results.books;
+        console.log(books)
         res.render('books/index', { allBooks: books })
       })  
 })
