adae5e70cd5ea8fed55b893541c6197a7792e651
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-29 14:19:12 -0600

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,9 +9,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/books', function(req, res) {
-    unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + NYT_API_KEY)
+    unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
       .end(function (response) {
         console.log(response.body);
+        res.end('Done')
       })
 })
 
